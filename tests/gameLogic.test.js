892359b68fb21ad463cc147d0e2c3883307a7ac1
Jesus Martinez <jesus.gomez5437@live.com>
2025-12-21 13:19:23 -0700

    Add test

--- tests/gameLogic.test.js
+++ tests/gameLogic.test.js
@@ -6,3 +6,9 @@ test("sanitizeName validates name and returns", () => {
         error: "Name can only contain letters, spaces, and hyphens."
     });
 });
+
+test("returns living expenses or 0", () => {
+    expect(GameLogic.addLivingExpenses(20, false)).toBe(24000);
+    expect(GameLogic.addLivingExpenses(17, false)).toBe(0);
+    expect(GameLogic.addLivingExpenses(22, true)).toBe(0);
+});
